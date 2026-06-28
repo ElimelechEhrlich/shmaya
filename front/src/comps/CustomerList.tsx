74eb5510b209e25e6c2374c69af06f39201f4ff2
ElimelechEhrlich <ee525566@gmail.com>
2026-06-28 19:17:38 +0300

    Update CustomerList.tsx

--- front/src/comps/CustomerList.tsx
+++ front/src/comps/CustomerList.tsx
@@ -61,7 +61,7 @@ const CustomerList: React.FC = () => {
     const filteredCustomers = useMemo(() => customers.filter(client => {
         const isApproved = finalizationMap.get(client.id) ?? false;
         const matchesSearch =const matchesSearch =
-    getCustomerDisplayName(client).includes(filters.search || '')
+    client.customerDetails?.fullName.includes(filters.search || '')
     || (client.businessDetails?.businessID || '').includes(filters.search || '');        const matchesType = filters.businessType === '' || client.businessDetails?.businessType === filters.businessType;
         const matchesInsurance = filters.isInsuranceActive === 'all' || String(!!client.isInsuranceActive) === filters.isInsuranceActive;
         const matchesTax = filters.isIncomeTaxActive === 'all' || String(!!client.isIncomeTaxActive) === filters.isIncomeTaxActive;
